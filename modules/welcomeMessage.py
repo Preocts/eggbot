6e89e84419dec3002eed754b5ac1f3113c80ccfa
preocts <preocts@gmail.com>
2020-03-27 20:28:15 -0400

    Create welcomeMessage module

--- modules/welcomeMessage.py
+++ modules/welcomeMessage.py
@@ -1,7 +1,8 @@
-""" GuildConfig is a module for Egg_Bot to control guild level configuration
+""" WelcomeMessage is a module for Egg_Bot for welcome messages
 
     Created by Preocts
     preocts@preocts.com | Preocts#8196 Discord
+    https://github.com/Preocts/Egg_Bot
 
     Refactor: 03/25/2020
 """
@@ -15,8 +16,8 @@ logger = logging.getLogger(__name__)  # Create module level logger
 VALID_KEYS_WELCOME = ('name', 'channel', 'roles', 'message')
 
 
-class eggGuildConfig:
-    """ Defines the eggGuildConfig class
+class welcomeMessage:
+    """ Defines the welcomeMessage class
 
         Config format:
         {
@@ -32,23 +33,24 @@ class eggGuildConfig:
         }
     """
 
-    def __init__(self, inputFile="./config/guildConfig.json"):
+    def __init__(self, inputFile="./config/welcomeMessage.json"):
         """ Define __init__ """
-        self.guildConfig = {}
+        logger.info(f'Initialize welcomeMessage: {inputFile}')
+        self.wmConfig = {}
         self.activeConfig = ''
         self.loadConfig(inputFile)
 
     def __str__(self):
-        return json.dumps(self.guildConfig, indent=4)
+        return json.dumps(self.wmConfig, indent=4)
 
     def __bool__(self):
-        if len(self.guildConfig):
+        if len(self.wmConfig):
             return True
         return False
 
     __nonzero__ = __bool__
 
-    def addWelcomeConfig(self, guildname: str, **kwargs) -> dict:
+    def addMessage(self, guildname: str, **kwargs) -> dict:
         """ Adds a welcome message to the loaded config
 
         Sets the configuration of welcome messages. If keyword 'channel' is
@@ -76,7 +78,7 @@ class eggGuildConfig:
             {"status": true/false, "response": str}
         """
 
-        logger.debug(f'addWelcomeConfig: {guildname} | {kwargs.items()}')
+        logger.debug(f'addMessage: {guildname} | {kwargs.items()}')
         config = {}
         for key, value in kwargs.items():
             if key in VALID_KEYS_WELCOME:
@@ -85,57 +87,72 @@ class eggGuildConfig:
             logger.error('"name" not provided for addWelcomeConfig')
             logger.debug(f'{guildname} | {kwargs.items()}')
             return {"status": False, "response": "Name not defined"}
-        if not(guildname in self.guildConfig):
-            self.guildConfig[guildname] = [config, ]
+        if not(guildname in self.wmConfig):
+            self.wmConfig[guildname] = [config, ]
         else:
-            for n in self.guildConfig[guildname]:
+            for n in self.wmConfig[guildname]:
                 if n["name"] == config["name"]:
                     logger.info(f'Name already exists: {config["name"]}')
                     return {"status": False, "response": "Name already exists"}
-            self.guildConfig[guildname].append(config)
+            self.wmConfig[guildname].append(config)
         logger.info(f'Guild Welcome added for {guildname}')
         logger.debug(f'{guildname} | {kwargs.items()}')
         return {"status": True, "response": "Welcome message created"}
 
-    def deleteWelcomeConfig(self, guildname: str, name: str) -> dict:
+    def getMessage(self, guildname: str, name: str) -> dict:
+        logger.debug(f'getMessage: {guildname} | {name}')
+        if not(guildname in self.wmConfig):
+            return {"status": False, "response": "Guild not found"}
+        for n in self.wmConfig[guildname]:
+            if n["name"] == name:
+                return {"status": True, "response": n}
+        return {"status": False, "response": "Name not found"}
+
+    def getMessages(self, guildname: str) -> dict:
+        logger.debug(f'getMessages: {guildname}')
+        if not(guildname in self.wmConfig):
+            return {"status": False, "response": "Guild not found"}
+        return{"status": True, "response": self.wmConfig[guildname]}
+
+    def deleteMessage(self, guildname: str, name: str) -> dict:
         """ Deletes a stored config for welcome messages
 
             Returns:
                 {"status": true/false, "response": str}
         """
-        logger.debug(f'deleteWelcomeConfig: {guildname} | {name}')
-        if not(guildname in self.guildConfig):
+        logger.debug(f'deleteMessage: {guildname} | {name}')
+        if not(guildname in self.wmConfig):
             return {"status": False, "response": "Guild not found"}
-        for n in self.guildConfig[guildname]:
+        for n in self.wmConfig[guildname]:
             if n["name"] == name:
                 logger.info(f'Deleting welcome message name: {name}')
-                i = self.guildConfig[guildname].index(n)
-                del self.guildConfig[guildname][i]
+                i = self.wmConfig[guildname].index(n)
+                del self.wmConfig[guildname][i]
                 return {"status": True, "response": "Welcome message deleted"}
         return {"status": False, "response": "Welcome message name not found"}
 
     def loadConfig(self, inputFile: str) -> dict:
-        """ Loads guildConfig configuration into memory"""
+        """ Loads welcomeMessage configuration into memory"""
 
         try:
             with open(inputFile) as file:
-                self.guildConfig = json.load(file)
+                self.wmConfig = json.load(file)
         except json.decoder.JSONDecodeError:
-            logger.error(f'guildConfig Config file empty ', exc_info=True)
+            logger.error(f'welcomeMessage Config file empty ', exc_info=True)
         except FileNotFoundError:
-            logger.error(f'guildConfig Config file not found '
-                         '{inputFile}', exc_info=True)
+            logger.error('welcomeMessage Config file not found '
+                         f'{inputFile}', exc_info=True)
             try:
                 open(inputFile, 'w')
             except OSError:
-                logger.critical('guildConfig failed to load. Closing. ',
+                logger.critical('welcomeMessage failed to load. Closing. ',
                                 exc_info=True)
                 exit()
         self.activeConfig = inputFile
         return {"status": True, "response": "Config loaded"}
 
     def saveConfig(self, outputFile: str = None) -> dict:
-        """ Writes guildConfig configuration to disk """
+        """ Writes welcomeMessage configuration to disk """
 
         if not(outputFile) and len(self.activeConfig) > 0:
             outputFile = self.activeConfig
@@ -144,12 +161,15 @@ class eggGuildConfig:
             return {"status": False, "response": "Missing filename"}
         try:
             with open(outputFile, 'w') as file:
-                file.write(json.dumps(self.guildConfig, indent=4))
-                logger.info(f'Success: guildConfig config '
-                            'saved to {outputFile}')
+                file.write(json.dumps(self.wmConfig, indent=4))
+                logger.info('Success: welcomeMessage config '
+                            f'saved to {outputFile}')
 
         except OSError:
-            logger.error(f'guildConfig Config file not saved to {outputFile}',
-                         exc_info=True)
+            logger.error('welcomeMessage Config file not saved to '
+                         f'{outputFile}', exc_info=True)
             return {"status": False, "response": "Error saving config"}
         return {"status": True, "response": "Config saved"}
+
+# May Bartmoss have mercy on your data for running this bot.
+# We are all only eggs
