0798783c5ad105630d25f35b41ba2e52ee6822dd
Preocts <preocts@gmail.com>
2020-11-27 23:54:09 -0500

    Restructure

---
+++ src/eggbot/__main__.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3.8
+# -*- encoding: utf-8 -*-
+""" Primary point of entry for Egg_Bot
+
+Author  : Preocts, preocts@preocts.com
+Discord : Preocts#8196
+Git Repo: https://github.com/Preocts/Egg_Bot
+"""
+from eggbot.eggbot_core import main
+
+
+if __name__ == '__main__':
+    main()
