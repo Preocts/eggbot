7940b3f85cc3d1bb6c3cdc52155fb0895bc75f0a
Preocts <preocts@gmail.com>
2021-04-17 00:44:23 -0400

    Get and save configs, Set 'max' attr
    Switched TypedDict to a NamedTuple for all of the benefits
    of a data-class that has default settings, is immutable, and
    just works better.

---
+++ modules/chatkudos.py
@@ -0,0 +1,146 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Kudos points brought to Discord
+
+Chat Kudos are points that can be granted, or taken, from server
+members by simply mentioning their name and having "+"s or "-"s
+following the mention.  The bot will reply with a customizable
+message, tell you how many kudos were just received, and keep a
+running tally.
+
+Author  : Preocts <preocts@preocts.com>
+Discord : Preocts#8196
+Git Repo: https://github.com/Preocts/Egg_Bot
+"""
+from __future__ import annotations
+import logging
+from typing import Any
+from typing import List
+from typing import Dict
+from typing import NamedTuple
+
+from discord import Message  # type: ignore
+
+from eggbot.configfile import ConfigFile
+
+MODULE_NAME: str = "ChatKudos"
+MODULE_VERSION: str = "1.0.0"
+DEFAULT_CONFIG: str = "configs/chatkudos.json"
+COMMAND_CONFIG: Dict[str, str] = {
+    "kudos!max": "set_max",
+    "kudos!gain": "set_gain",
+    "kudos!loss": "set_loss",
+}
+
+
+class KudosConfig(NamedTuple):
+    """ Config model for a guild in ChatKudos """
+
+    roles: List[str] = []
+    users: List[str] = []
+    max: int = 5
+    lock: bool = False
+    gain_message: str = "[POINTS] to [NICKNAME]! That gives them [TOTAL] total!"
+    loss_message: str = "[POINTS] from [NICKNAME]! That leaves them [TOTAL] total!"
+    scores: Dict[str, int] = {}
+
+    @classmethod
+    def from_dict(cls, config: Dict[str, Any]) -> KudosConfig:
+        """ Create model from loaded config segment """
+        return cls(
+            roles=config.get("roles", []),
+            users=config.get("users", []),
+            max=config.get("max", 5),
+            lock=config.get("lock", False),
+            gain_message=config.get(
+                "gain_message",
+                "[POINTS] to [NICKNAME]! That gives them [TOTAL] total!",
+            ),
+            loss_message=config.get(
+                "loss_message",
+                "[POINTS] from [NICKNAME]! That gives them [TOTAL] total!",
+            ),
+            scores=config.get("scores", {}),
+        )
+
+    def as_dict(self) -> Dict[str, Any]:
+        """ Returns NamedTuple as Dict """
+        return self._asdict()  # pylint: disable=E1101
+
+
+class ChatKudos:
+    """ Kudos points brought to Discord """
+
+    logger = logging.getLogger(__name__)
+
+    def __init__(self, config_file: str = DEFAULT_CONFIG) -> None:
+        """ Create instance and load configuration file """
+        self.logger.info("Initializing ChatKudos module")
+        self.config = ConfigFile()
+        self.config.load(config_file)
+        if not self.config.config:
+            self.config.create("module", MODULE_NAME)
+            self.config.create("version", MODULE_VERSION)
+        else:
+            if self.config.read("module") != MODULE_NAME:
+                self.logger.warning("ChatKudos config module name mismatch!")
+            if self.config.read("version") != MODULE_VERSION:
+                self.logger.warning("ChatKudos config version mismatch!")
+
+    def get_guild(self, guild_id: str) -> KudosConfig:
+        """ Load a guild from the config, return defaults if empty """
+        self.logger.debug("Get guild '%s'", guild_id)
+        guild_conf = self.config.read(guild_id)
+        if not guild_conf:
+            return KudosConfig()
+        return KudosConfig.from_dict(guild_conf)
+
+    def save_guild(self, guild_id: str, **kwargs: Any) -> None:
+        """
+        Save a guild entry. Any keyword excluded will save existing value.
+
+        Keyword Args:
+            roles: List[str], roles that can use when locked
+            users: List[str], users that can use when locked
+            max: int, max points granted in one line
+            lock: bool, restict to `roles`/`users` or open to all
+            gain_message: str, message displayed on gain of points
+            loss_message: str, message displayed on loss of points
+        """
+        self.logger.debug("Save: %s, (%s)", guild_id, kwargs)
+        guild_conf = self.get_guild(guild_id)
+        new_conf = KudosConfig(
+            roles=kwargs.get("roles", guild_conf.roles),
+            users=kwargs.get("users", guild_conf.users),
+            max=kwargs.get("max", guild_conf.max),
+            lock=kwargs.get("lock", guild_conf.lock),
+            gain_message=kwargs.get("gain_message", guild_conf.gain_message),
+            loss_message=kwargs.get("loss_message", guild_conf.loss_message),
+        )
+        if not self.config.read(guild_id):
+            self.config.create(guild_id, new_conf.as_dict())
+        else:
+            self.config.update(guild_id, new_conf.as_dict())
+
+    def set_max(self, message: Message) -> str:
+        """ Set max number of points to be gained in one line """
+        self.logger.debug("Set %s max: %s", message.guild.name, message.clean_content)
+        try:
+            max_ = int(message.clean_content.replace("kudos!max", ""))
+        except ValueError:
+            return "Usage: `kudo!max [N]` where N is a number."
+        self.save_guild(str(message.guild.id), max=max_)
+        if max_ > 0:
+            return f"Max points set to {max_}"
+        return "Max points set to unlimited"
+
+    def parse_command(self, message: Message) -> str:
+        """ Process all commands prefixed with 'kudos!' """
+        self.logger.debug("Parsing command: %s", message.clean_content)
+        command = message.clean_content.split()[0]
+        try:
+            return getattr(self, COMMAND_CONFIG[command])(message)
+        except (AttributeError, KeyError):
+            self.logger.error("'%s' attribute not found!", command)
+        return ""
---
+++ tests/test_module_chatkudos.py
@@ -0,0 +1,129 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Unit tests for ./modules/chatkudos.py
+
+To run these tests from command line use the following:
+    $ python -m pytest -v testes/test_module_chatkudos.py
+
+Author  : Preocts <preocts@preocts.com>
+Discord : Preocts#8196
+Git Repo: https://github.com/Preocts/Egg_Bot
+"""
+from unittest.mock import Mock
+
+import pytest
+
+from modules.chatkudos import ChatKudos
+from modules.chatkudos import MODULE_NAME
+from modules.chatkudos import MODULE_VERSION
+from modules.chatkudos import COMMAND_CONFIG
+
+
+@pytest.fixture(scope="function", name="kudos")
+def fixture_kudos() -> ChatKudos:
+    """ Fixture """
+    return ChatKudos("./tests/fixtures/mock_chatkudos.json")
+
+
+@pytest.fixture(scope="function", name="message")
+def fixture_message() -> Mock:
+    """ Fixture """
+    message = Mock()
+    message.clean_content = "kudos!help"
+    message.guild.name = "Testing Guild"
+    message.guild.id = 111
+    message.author.id = 111
+    message.author.display_name = "Tester"
+    return message
+
+
+def test_does_fixture_need_updating(kudos: ChatKudos) -> None:
+    """ Confirm version and module names match, sanity check """
+    assert kudos.config.read("module") == MODULE_NAME
+    assert kudos.config.read("version") == MODULE_VERSION
+
+
+def test_command_config(kudos: ChatKudos) -> None:
+    """ Ensure all commands have attr that exist """
+    for attr in COMMAND_CONFIG.values():
+        assert getattr(kudos, attr, None) is not None, f"Missing {attr} attribute"
+
+
+def test_load_guild_found(kudos: ChatKudos) -> None:
+    """ Load a guild that exists, check values """
+    result = kudos.get_guild("111")
+    assert result.max == -1
+    assert result.users
+    assert not result.roles
+
+
+def test_load_guild_not_found(kudos: ChatKudos) -> None:
+    """ Load a guild that doesn't exist, check defaults """
+    result = kudos.get_guild("999")
+    assert result.max == 5
+    assert not result.users
+
+
+def test_save_guild_exists(kudos: ChatKudos) -> None:
+    """ Save explict changes, confirm existing don't change """
+    kudos.save_guild("111", max=-1, gain_message="TEST")
+    kudos.save_guild("111", loss_message="TEST02")
+
+    assert kudos.get_guild("111").max == -1
+    assert kudos.get_guild("111").gain_message == "TEST"
+    assert kudos.get_guild("111").loss_message == "TEST02"
+
+
+def test_save_guild_not_exists(kudos: ChatKudos) -> None:
+    """ Save changes to a guild that does't exist """
+    kudos.save_guild("999", max=-1, gain_message="TEST")
+    kudos.save_guild("999", loss_message="TEST02")
+
+    assert kudos.get_guild("999").max == -1
+    assert kudos.get_guild("999").gain_message == "TEST"
+    assert kudos.get_guild("999").loss_message == "TEST02"
+
+
+def test_adjust_max(kudos: ChatKudos, message: Mock) -> None:
+    """ Change max for existing and non-existing guild """
+    message.clean_content = "kudos!max 10"
+    result = kudos.parse_command(message)
+    assert "Max points set to 10" in result
+
+    message.clean_content = "kudos!max -1"
+    message.guild.id = "999"
+    result = kudos.parse_command(message)
+    assert "Max points set to unlimited" in result
+
+    assert kudos.get_guild("111").max == 10
+    assert kudos.get_guild("999").max == -1
+
+
+def test_adjust_max_invalid(kudos: ChatKudos, message: Mock) -> None:
+    """ Provide an invalid format to the command """
+    message.clean_content = "kudos!max 10 points"
+    result = kudos.parse_command(message)
+    assert "Usage:" in result
+
+
+def test_adjust_gain_messages(kudos: ChatKudos, message: Mock) -> None:
+    """ Adjust the gain messages on existing and non-existing guilds """
+    message.clean_content = "kudos!gain This is gain"
+    result = kudos.parse_command(message)
+    assert "Gain message set: `This is gain`" in result
+
+    message.guild.id = "999"
+    result = kudos.parse_command(message)
+    assert "Gain message set: `This is gain`" in result
+
+
+def test_adjust_loss_messages(kudos: ChatKudos, message: Mock) -> None:
+    """ Adjust the loss messages on existing and non-existing guilds """
+    message.clean_content = "kudos!loss This is loss"
+    result = kudos.parse_command(message)
+    assert "Loss message set: `This is loss`" in result
+
+    message.guild.id = "999"
+    result = kudos.parse_command(message)
+    assert "Loss message set: `This is loss`" in result
